2dc659d19e3ee258c1fa56d07a1d6cd3203975a6
Jose Cruz <floobot@hotmail.com>
2022-01-07 19:02:36 -0600

    coding main Add functions for read numbers from file

--- day1.c
+++ day1.c
@@ -1,25 +1,38 @@
 #include<stdio.h>
+#include <stdlib.h>
+
+// char[] readFile()
+
+int intToChar(char* stringNumber) {
+  return atoi(stringNumber);
+}
 
 int validateNumber(int n1, int n2) {
   return n2 > n1 ? 1 : 0;
 }
 
 void showNumbers(int* list, int size) {
-  printf("%i", list[0]);
   int result=0;
   for(int i = 0; i < size-1; i++) {
-    printf("%i\n", list[i]);
     result += validateNumber(list[i], list[i+1]);
-
   }
   printf("%i\n",result);
 }
 
 int main() {
-  int numbers[10] = {
-    199,200,208,210,200,207,240,269,260,263
-  };
-  char strings[3][3] = {"199","200","208"};
-  showNumbers(numbers, 10);
+  FILE* input_file = fopen("input.txt", "r");
+  if(!input_file)
+    exit(EXIT_FAILURE);
+
+  char *contents = NULL;
+  size_t len = 0;
+  int lines = 0;
+  int*  numbers=(int*) malloc(sizeof(int)*1);
+  while(getline(&contents, &len, input_file) != -1) {
+    numbers[lines++] = atoi(contents);
+    numbers = realloc(numbers, sizeof(int)*lines);
+    printf("%s\n", contents);
+  }
+  showNumbers(numbers, lines);
   return 0;
 }
